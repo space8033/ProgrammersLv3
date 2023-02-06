0278cf4ae0b4e877122726fbbcdd18a270955098
space8033 <space8237@naver.com>
2023-02-06 10:07:17 +0900

    0206 done

---
+++ greedy/DeliveryAndCollect.java
@@ -0,0 +1,25 @@
+public class DeliveryAndCollect {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = 0;
+        int del = 0;
+        int pick = 0;
+
+        for(int i = n-1; i >= 0; i--) {
+            if(deliveries[i] != 0 || pickups[i] != 0) {
+                int cnt = 0;
+                while (del < deliveries[i] || pick < pickups[i]) {
+                    cnt++;
+                    del += cap;
+                    pick += cap;
+                }
+
+                del -= deliveries[i];
+                pick -= pickups[i];
+
+                answer += (long) (i + 1) * cnt * 2;
+            }
+        }
+
+        return answer;
+    }
+}
